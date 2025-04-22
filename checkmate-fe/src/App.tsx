c37ef8ace59321fe4cb4da64661a8d1abcef5b94
newww-a <tls001217@naver.com>
2025-04-22 11:33:12 +0900

    feat: 초기 설정 -pwa - tailwindcss

---
+++ checkmate-fe/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-3xl font-bold'>
+          tailwind testds
+      </div>
+    </>
+  )
+}
+
+export default App

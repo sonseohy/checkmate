e5cff9aaa1201cfdfd0b0e038439f631e3f424e5
leeyj <joker901010@gmail.com>
2025-04-24 00:02:53 +0900

    feat: 시큐리티 추가 및 엔터티 컬럼 추가

--- checkmate-be/src/main/java/com/checkmate/global/common/filter/JwtAuthenticationFilter.java
+++ checkmate-be/src/main/java/com/checkmate/global/common/filter/JwtAuthenticationFilter.java
@@ -11,7 +11,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;

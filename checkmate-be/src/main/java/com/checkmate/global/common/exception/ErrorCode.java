0551c95f0a8184e81f423d6acd59c683b6d1a5fa
sonseohy <sonseoh223@gmail.com>
2025-04-28 01:23:42 +0900

    feat: ErrorCode 추가

--- checkmate-be/src/main/java/com/checkmate/global/common/exception/ErrorCode.java
+++ checkmate-be/src/main/java/com/checkmate/global/common/exception/ErrorCode.java
@@ -15,7 +15,13 @@ public enum ErrorCode {
     INVALID_TOKEN("AUTH-003", HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
     TOKEN_STORAGE_FAILED("AUTH-004", HttpStatus.INTERNAL_SERVER_ERROR, "토큰 저장 중 오류가 발생했습니다."),
     INVALID_REFRESH_TOKEN("AUTH-005", HttpStatus.UNAUTHORIZED, "Redis 리프레시 토큰과 다릅니다."),
-            ;
+
+    ENCRYPT_FAILED("ENCRYPT-001", HttpStatus.INTERNAL_SERVER_ERROR, "암호화에 실패했습니다."),
+    DECRYPT_FAILED("ENCRYPT-002", HttpStatus.INTERNAL_SERVER_ERROR, "복호화에 실패했습니다."),
+
+    USER_NOT_FOUND("USER-001", HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
+    USER_DELETED("USER-002", HttpStatus.BAD_REQUEST, "탈퇴한 회원입니다."),
+    ;
 
     private final String code;
     private final HttpStatus httpStatus;

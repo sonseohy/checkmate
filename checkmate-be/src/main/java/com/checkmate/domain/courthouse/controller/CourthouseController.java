c3349f34ede95e5707d4f771dbd4ff96008cc2d9
김성찬 <hjkim2661@gmail.com>
2025-04-29 16:41:23 +0900

    feat: 법원 정보 조회 기능 구현

--- checkmate-be/src/main/java/com/checkmate/domain/courthouse/controller/CourthouseController.java
+++ checkmate-be/src/main/java/com/checkmate/domain/courthouse/controller/CourthouseController.java
@@ -1,10 +1,14 @@
 package com.checkmate.domain.courthouse.controller;
 
+import java.util.List;
+
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.checkmate.domain.courthouse.dto.response.CourthouseResponseDto;
+import com.checkmate.domain.courthouse.service.CourthouseService;
 import com.checkmate.global.common.response.ApiResponse;
 
 import lombok.RequiredArgsConstructor;
@@ -14,10 +18,16 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api")
 public class CourthouseController {
 
+	private final CourthouseService courthouseService;
+	/**
+	 * 법원 정보 조회
+	 *
+	 * @return 전국 법원 정보(법원 이름, 주소, 대표 번호)
+	 */
 	@GetMapping("/courthouses")
-	public ApiResponse<CourthouseResponseDto> getCourthouse() {
-
-
-		return ApiResponse.ok();
+	// @PreAuthorize("isAuthenticated()")
+	public ApiResponse<List<CourthouseResponseDto>> getCourthouse() {
+		List<CourthouseResponseDto> data = courthouseService.getAllCourthouseInfo();
+		return ApiResponse.ok(data);
 	}
 }
--- checkmate-be/src/main/java/com/checkmate/global/common/exception/ErrorCode.java
+++ checkmate-be/src/main/java/com/checkmate/global/common/exception/ErrorCode.java
@@ -15,6 +15,8 @@ public enum ErrorCode {
     INVALID_TOKEN("AUTH-003", HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
     TOKEN_STORAGE_FAILED("AUTH-004", HttpStatus.INTERNAL_SERVER_ERROR, "토큰 저장 중 오류가 발생했습니다."),
     INVALID_REFRESH_TOKEN("AUTH-005", HttpStatus.UNAUTHORIZED, "Redis 리프레시 토큰과 다릅니다."),
+
+    COURTHOUSE_NOT_FOUND("COURT-001", HttpStatus.NOT_FOUND, "법원 정보를 찾을 수 없습니다."),
             ;
 
     private final String code;

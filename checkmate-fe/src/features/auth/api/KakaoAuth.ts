ded580a5be948e06f7882e706f011e90b5488604
newww-a <tls001217@naver.com>
2025-05-02 01:03:01 +0900

    fix: 소셜 로그인 회원가입 로직 수정중

--- checkmate-fe/src/features/auth/api/KakaoAuth.ts
+++ checkmate-fe/src/features/auth/api/KakaoAuth.ts
@@ -2,5 +2,8 @@
 import { customAxios } from "@/shared/api/client/customAxios";
 
 export function PostKakaoCallback(code: string) {
-    return customAxios.post("/api/auth/kakao/callback", { code });
-  }
+  return customAxios.post('/api/auth/login', {
+    providerId: code,
+
+  })
+}

fc204f50d1f5594ad2f6d0563e47c82dbc8ff50d
손서현 <sonseoh223@gmail.com>
2025-05-08 01:33:44 +0900

    feat: 카테고리 포함한 Template 조회 메서드 추가

--- checkmate-be/src/main/java/com/checkmate/domain/template/repository/TemplateRepository.java
+++ checkmate-be/src/main/java/com/checkmate/domain/template/repository/TemplateRepository.java
@@ -2,6 +2,12 @@ package com.checkmate.domain.template.repository;
 
 import com.checkmate.domain.template.entity.Template;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface TemplateRepository extends JpaRepository<Template, Integer> {
+    @Query("SELECT t FROM Template t LEFT JOIN FETCH t.category WHERE t.id = :id")
+    Optional<Template> findByIdWithCategory(@Param("id") Integer id);
 }

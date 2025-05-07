b25c6ddd84b164973e5ef24b495c7b012a79f443
손서현 <sonseoh223@gmail.com>
2025-05-08 01:36:23 +0900

    feat: Section 조회 메서드 추가

--- checkmate-be/src/main/java/com/checkmate/domain/section/repository/SectionRepository.java
+++ checkmate-be/src/main/java/com/checkmate/domain/section/repository/SectionRepository.java
@@ -2,6 +2,13 @@ package com.checkmate.domain.section.repository;
 
 import com.checkmate.domain.section.entity.Section;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface SectionRepository extends JpaRepository<Section, Integer> {
+
+    @Query("SELECT s FROM Section s JOIN s.templateSections ts WHERE ts.template.id = :templateId ORDER BY ts.templateSectionNo ASC")
+    List<Section> findSectionsByTemplateId(@Param("templateId") Integer templateId);
 }

2b857013daa839ee6c71966bcfacb0d179e36850
손서현 <sonseoh223@gmail.com>
2025-05-08 01:36:45 +0900

    feat: Template 조회 API 구현

--- checkmate-be/src/main/java/com/checkmate/domain/template/controller/TemplateController.java
+++ checkmate-be/src/main/java/com/checkmate/domain/template/controller/TemplateController.java
@@ -1,11 +1,23 @@
 package com.checkmate.domain.template.controller;
 
+import com.checkmate.domain.template.dto.response.TemplateResponseDto;
+import com.checkmate.domain.template.service.TemplateService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/templates")
 @RequiredArgsConstructor
 public class TemplateController {
+    private final TemplateService templateService;
+
+    @GetMapping("/{templateId}")
+    public ResponseEntity<TemplateResponseDto> getTemplate(@PathVariable Integer templateId) {
+        TemplateResponseDto response = templateService.getTemplate(templateId);
+        return ResponseEntity.ok(response);
+    }
 }

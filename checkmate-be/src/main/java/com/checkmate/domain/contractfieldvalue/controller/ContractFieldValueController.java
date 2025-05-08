5a72b239799a6c71c891164a1ddaa87757730701
손서현 <sonseoh223@gmail.com>
2025-05-09 02:42:08 +0900

    feat: 계약서 필드 값 저장 API 추가

--- checkmate-be/src/main/java/com/checkmate/domain/contractfieldvalue/controller/ContractFieldValueController.java
+++ checkmate-be/src/main/java/com/checkmate/domain/contractfieldvalue/controller/ContractFieldValueController.java
@@ -1,11 +1,25 @@
 package com.checkmate.domain.contractfieldvalue.controller;
 
+import com.checkmate.domain.contractfieldvalue.dto.request.ContractFieldValueRequestDto;
+import com.checkmate.domain.contractfieldvalue.dto.response.ContractFieldValueResponseDto;
+import com.checkmate.domain.contractfieldvalue.service.ContractFieldValueService;
+import com.checkmate.global.common.response.ApiResult;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/contracts")
 @RequiredArgsConstructor
 public class ContractFieldValueController {
+    private final ContractFieldValueService contractFieldValueService;
+
+    @PostMapping("/{contractId}/inputs")
+    public ApiResult<ContractFieldValueResponseDto> saveFieldValues(
+            @PathVariable Integer contractId,
+            @Valid @RequestBody ContractFieldValueRequestDto request) {
+
+        ContractFieldValueResponseDto response = contractFieldValueService.saveFieldValues(contractId, request);
+        return ApiResult.ok(response);
+    }
 }

e282c20434f301975e0c397b54cbc3996bee41f6
songowen <alslvls1995@naver.com>
2025-05-12 16:46:52 +0900

    fix: chat 이력 로컬스토리지에 userid별로 저장

--- checkmate-fe/src/features/auth/slices/authSlice.ts
+++ checkmate-fe/src/features/auth/slices/authSlice.ts
@@ -2,25 +2,25 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { AuthState, UserInfo } from '@/features/auth';
 
 const initialState: AuthState = {
-    isAuthenticated: false,
-    user: null,
+  isAuthenticated: false,
+  user: null,
 };
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {
-        loginSuccess(state, action: PayloadAction<UserInfo>) {
-            state.isAuthenticated = true;
-            state.user = action.payload; //로그인한 사용자 정보
-        },
-        logout(state) {
-            state.isAuthenticated = false; //로그아웃 시 사용자 정보 초기화
-            state.user = null;
-        },
+  name: 'auth',
+  initialState,
+  reducers: {
+    loginSuccess(state, action: PayloadAction<UserInfo>) {
+      state.isAuthenticated = true;
+      state.user = action.payload; //로그인한 사용자 정보
     },
+    logout(state) {
+      state.isAuthenticated = false; //로그아웃 시 사용자 정보 초기화
+      state.user = null;
+    },
+  },
 });
 
 export const { loginSuccess, logout } = authSlice.actions;
-    
+
 export default authSlice.reducer;

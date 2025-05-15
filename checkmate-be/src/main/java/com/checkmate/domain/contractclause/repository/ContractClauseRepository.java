422c652cc9b643ef80a44207ee54bb28fd03d2ff
leeyj <joker901010@gmail.com>
2025-05-15 10:26:56 +0900

    fix: entity-drop

--- checkmate-be/src/main/java/com/checkmate/domain/contractclause/repository/ContractClauseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.checkmate.domain.contractclause.repository;
-
-import com.checkmate.domain.contractclause.entity.ContractClause;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContractClauseRepository extends JpaRepository<ContractClause, Integer> {
-}
--- checkmate-be/src/main/java/com/checkmate/domain/ocrresult/repository/OcrResultRepository.java
+++
@@ -1,7 +0,0 @@
-package com.checkmate.domain.ocrresult.repository;
-
-import com.checkmate.domain.ocrresult.entity.OcrResult;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OcrResultRepository extends JpaRepository<OcrResult, Integer> {
-}

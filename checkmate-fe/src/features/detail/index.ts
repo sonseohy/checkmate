97dd1b04ed24d48aa3999440c4bc078604fac8a8
songowen <alslvls1995@naver.com>
2025-05-18 14:10:54 +0900

    feat: 알림창에서 전자서명 알림 클릭시 계약서작성 디테일 페이지로 이동

--- checkmate-fe/src/features/detail/index.ts
+++ checkmate-fe/src/features/detail/index.ts
@@ -1,15 +1,13 @@
-
-
 //api
-export { getContractDetail, 
-         getContractQuestions, 
-         getContractownload, 
-         deleteContractDetail } from "./api/detailApi";
+export {
+  getContractDetail,
+  getContractQuestions,
+  getContractownload,
+  deleteContractDetail,
+} from './api/detailApi';
 
-
-export { default as ContractPdfViewer } from "./ui/ContractPdfViewer";
-export { default as ContractDetail } from "./ui/ContractDetail";
+export { default as ContractPdfViewer } from './ui/ContractPdfViewer';
+export { default as ContractDetail } from './ui/ContractDetail';
 
 //type
-export type { questions, 
-              questionList } from './model/types';
+export type { questions, questionList, ContractSummary } from './model/types';

3a7cfec951bb1a98acc6d74b0bec8dfa461d511f
김성찬 <hjkim2661@gmail.com>
2025-05-20 11:53:09 +0900

    refactor: 주석 추가

--- checkmate-be/src/main/java/com/checkmate/domain/chatbot/controller/ChatbotController.java
+++ checkmate-be/src/main/java/com/checkmate/domain/chatbot/controller/ChatbotController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.checkmate.domain.chatbot.service.OpenAIService;
 import com.checkmate.global.common.response.ApiResult;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
@@ -16,6 +20,7 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/api/chatbot")
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "Chatbot API", description = "챗봇 API")
 public class ChatbotController {
 	private final OpenAIService openAIService;
 
@@ -25,6 +30,10 @@ public class ChatbotController {
 	 * @param message 질문 메세지
 	 * @return gpt 응답 내용
 	 */
+	@Operation(summary = "챗봇 채팅", description = "챗봇과 채팅합니다.")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "챗봇 채팅 성공"),
+	})
 	@PostMapping
 	public Mono<ApiResult<String>> chatWithSession(
 		@RequestBody String message) {
